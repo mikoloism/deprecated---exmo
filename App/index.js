dd6daacb236c10c7ab004cd67d8547a461bd591a
miko <mikoloism.github@gmail.com>
2020-10-22 18:27:00 +0330

    Update /App/index.js
    Added classic way to use database
    
    ## ISSUES:
    - [x] structure successfully work 🎉
    - [ ] connect to database or use database not work!!! 👎

--- App/index.js
+++ App/index.js
@@ -1,3 +1,30 @@
-module.exports = () => {
-	return '/App';
+const MongoClient = require('mongodb').MongoClient;
+
+class ExMo {
+	constructor(db) {
+		this.dbName = db;
+		this.url = `mongodb://localhost:27017/`;
+		this.bound = this.bound.bind(this);
+		return this.bound();
+	}
+	bound() {
+		this.init = this.init.bind(this);
+		return this.init;
+	}
+
+	init(table) {
+		this.tableName = table;
+		const connect = (err, connection) => {
+			if (err) throw err;
+			this.db = connection.db(this.dbName);
+			this.table = this.db.collection(this.tableName);
+			connection.close();
+		};
+		MongoClient.connect(this.url, { useUnifiedTopology: true }, connect);
+		return this; // should be `this.table` as `connection.db(...).collection(...);`
+	}
+}
+
+module.exports = (db_name) => {
+	return new ExMo(db_name);
 };
