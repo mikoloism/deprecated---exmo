c09868065ea6867af01a3c3ce7faabb7be99d7ba
miko <mikoloism.github@gmail.com>
2020-10-22 18:17:54 +0330

    Update index.js
    added function way to connect into database
    
    ## ISSUES:
    - [x] Structur done
    - [ ]  MongoDB (MongoClient) Not work!!!!

--- App/index.js
+++ App/index.js
@@ -1,3 +1,24 @@
-module.exports = () => {
-	return '/App';
+const MongoClient = require('mongodb').MongoClient;
+
+function init(db, table) {
+	const _ = {};
+	_.url = 'mongodb://localhost:27017';
+	_.tableName = table;
+	_.dbName = db;
+	MongoClient.connect(_.url, { useUnifiedTopology: true }, function (
+		err,
+		client,
+	) {
+		if (err) throw err;
+		_.db = client.db(_.dbName);
+		_.table = client.db(_.dbName).collection(_.tableName);
+		client.close();
+	});
+
+	return _; // should be `_.table` as `client.db(...).collection(...);`
+}
+
+module.exports = (db_name) => {
+	init = init.bind(init, db_name);
+	return init;
 };
